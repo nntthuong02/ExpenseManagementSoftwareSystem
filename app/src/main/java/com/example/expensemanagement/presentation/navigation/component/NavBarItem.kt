9df703e9932661a4dec664bb7229599e4f79d254
nntthuong <thuong.nn204855@gmail.com>
2024-07-18 10:20:03 +0700

    navigation.component

---
+++ app/src/main/java/com/example/expensemanagement/presentation/navigation/component/NavBarItem.kt
@@ -0,0 +1,89 @@
+package com.example.expensemanagement.presentation.navigation.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.expensemanagement.R
+import com.example.expensemanagement.ui.theme.ExpenseManagementTheme
+
+
+@ExperimentalAnimationApi
+@Composable
+fun NavBarItem(item: NavBarItemHolder, onClick: () -> Unit, selected: Boolean) {
+
+    val backgroundColor =
+        if (selected) MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f) else Color.Transparent
+    val contentColor =
+        if (selected) MaterialTheme.colorScheme.onSurface else Color.Gray
+
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(backgroundColor)
+            .clickable { onClick() }
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 12.dp, horizontal = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+
+        ) {
+            Icon(
+                painterResource(id = item.icon),
+                tint = contentColor,
+                contentDescription = item.title
+            )
+            AnimatedVisibility(visible = selected) {
+                Text(
+                    text = item.title,
+                    textAlign = TextAlign.Center,
+                    color = contentColor,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+@Preview(showBackground = true)
+@Composable
+fun NavBarItemPreview() {
+    val item = NavBarItemHolder(
+        title = "Home",
+        icon = R.drawable.home_24px,
+        route = "home"
+    )
+    val selected = remember { mutableStateOf(true) }
+
+    ExpenseManagementTheme {
+        NavBarItem(
+            item = item,
+            onClick = { selected.value = !selected.value },
+            selected = selected.value
+        )
+    }
+}
---
+++ app/src/main/java/com/example/expensemanagement/presentation/navigation/component/NavBarItemHolder.kt
@@ -0,0 +1,34 @@
+package com.example.expensemanagement.presentation.navigation.component
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.expensemanagement.R
+import com.example.expensemanagement.presentation.navigation.Route
+
+data class NavBarItemHolder(
+    val title: String,
+    val icon: Int,
+    val route: String
+)
+
+fun provideBottomNavItems() = listOf(
+    NavBarItemHolder(
+        "Home",
+        R.drawable.home_24px,
+        Route.HomeScreen.route
+    ),
+    NavBarItemHolder(
+        "Transaction",
+        R.drawable.transaction_24px,
+        Route.TransactionScreen.route
+    ),
+    NavBarItemHolder(
+        "Insight",
+        R.drawable.insight_24,
+        Route.InsightScreen.route
+    ),
+    NavBarItemHolder(
+        "Payment",
+        R.drawable.payments_24px,
+        Route.PayScreen.route
+    )
+)
